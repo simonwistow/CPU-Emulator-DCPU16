231d87efae24ed2fc42ca58172d8f4110c575f55
Simon Wistow <simon@thegestalt.org>
2012-04-16 16:44:58 -0700

    Stub out

--- lib/CPU/Emulator/DCPU16/Assembler.pm
+++ lib/CPU/Emulator/DCPU16/Assembler.pm
@@ -1,3 +1,9 @@
 package CPU::Emulator::DCPU16::Assembler;
 
+=head1 NAME
+
+CPU::Emulator::DCPU16::Assembler -  assemble DCPU-16 bytecode
+
+=cut
+
 1;
--- lib/CPU/Emulator/DCPU16/Disassembler.pm
+++ lib/CPU/Emulator/DCPU16/Disassembler.pm
@@ -1,3 +1,9 @@
 package CPU::Emulator::DCPU16::Disassembler;
 
+=head1 NAME
+
+CPU::Emulator::DCPU16::Disassembler -  a disassembler for DCPU-16 bytecode
+
+=cut
+
 1;
